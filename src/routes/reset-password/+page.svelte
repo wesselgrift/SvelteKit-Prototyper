3b621f9d51f2410ce6974c75c2f0d4d7623bbe0f
Wessel Grift <wesselgrift@users.noreply.github.com>
2025-01-27 14:03:29 +0100

    add logo to reset pw screen

--- src/routes/reset-password/+page.svelte
+++ src/routes/reset-password/+page.svelte
@@ -7,6 +7,7 @@
   
 <div class="flex justify-center" style="padding-top: 120px;">
   <div style="max-width: 470px;" class="p-5 w-full">
+    <img src="/logo.svg" alt="Google" class="h-8 mb-10" />
     <h2 class="text-3xl font-semibold mb-9 leading-tight">Reset Password</h2>
     <ResetPassword />
     <a href="/login" class="flex items-center justify-center gap-1 mt-5 text-sm text-gray-500 hover:text-gray-700 transition">
