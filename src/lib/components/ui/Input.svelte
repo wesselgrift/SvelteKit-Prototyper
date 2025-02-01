8062dba3bec8b4f1cf305a955604189d067c8e0e
Wessel Grift <wesselgrift@users.noreply.github.com>
2025-02-01 17:02:36 +0100

    update mobile components

--- src/lib/components/ui/Input.svelte
+++ src/lib/components/ui/Input.svelte
@@ -3,13 +3,13 @@
       name, 
       value = $bindable(), 
       type = 'text',
-      marginBottom = 'mb-4',
+      marginBottom = 'mb-3 lg:mb-4',
     } = $props();
 </script>
   
   <input
     {name}
     {type}
-    class="{marginBottom} block w-full text-base py-3 px-4 border border-1 border-color-input rounded-lg transition focus:border-color-input focus:ring-2 focus:ring-color-ring focus:ring-offset-2"
+    class="{marginBottom} block w-full text-base py-2 px-3.5 lg:py-3 lg:px-4 border border-1 border-color-input rounded-lg transition focus:border-color-input focus:ring-2 focus:ring-color-ring focus:ring-offset-2"
     bind:value
   />
