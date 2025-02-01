010e4e684cdaf6cde4a7c485691611d72486dbac
Wessel Grift <wesselgrift@users.noreply.github.com>
2025-02-01 12:06:48 +0100

    rm button

--- src/routes/app/page-3/+page.svelte
+++ src/routes/app/page-3/+page.svelte
@@ -1,12 +1,5 @@
 <script>
     import Button from "$lib/components/ui/Button.svelte";
-
-    function handleClick() {
-        alert("clicked");
-    }
 </script>
 
 <h1 class="text-2xl font-semibold mb-5 text-color-foreground">Page 3</h1>
-<div class="w-64">
-    <Button variant="secondary" onclick={handleClick}>Click me</Button>
-</div>
