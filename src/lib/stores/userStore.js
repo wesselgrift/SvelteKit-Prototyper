b0c6084a1e4dfeaeb1fd02614d16af2995ac7285
Wessel Grift <wesselgrift@users.noreply.github.com>
2025-03-21 21:01:03 +0100

    comments

--- src/lib/stores/userStore.js
+++ src/lib/stores/userStore.js
@@ -1,5 +1,7 @@
 import { writable } from "svelte/store";
 
+// On auth state change user is set
+// Loading is set to true when the auth state is being checked
 export const user = writable(null);
 export const loading = writable(true);
 
