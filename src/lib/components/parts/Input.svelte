00a88746046e7a7ecba4b62471083b242ab3e0b5
Wessel Grift <wesselgrift@users.noreply.github.com>
2025-03-26 22:22:13 +0100

    fix

--- src/lib/components/parts/Input.svelte
+++ src/lib/components/parts/Input.svelte
@@ -7,7 +7,7 @@
     } = $props();
 
     const defaultClasses = 
-    "block w-full text-base lg:text-sm py-2 px-3.5 lg:py-3 lg:px-4 border border-border focus:border-border bg-background rounded-lg ring transition-all";
+    "block w-full text-base lg:text-sm py-2 px-3.5 lg:py-3 lg:px-4 border border-input focus:border-border bg-background rounded-lg ring transition-all";
 </script>
   
 <input {name} {type} class={defaultClasses} bind:value />
