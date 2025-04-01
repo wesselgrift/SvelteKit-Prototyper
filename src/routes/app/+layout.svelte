3cc8ee654d85442b7d7ed98a52b2f2bad8417751
Wessel Grift <wesselgrift@users.noreply.github.com>
2025-04-01 20:17:40 +0200

    fix overflow behaviour on mobile

--- src/routes/app/+layout.svelte
+++ src/routes/app/+layout.svelte
@@ -14,23 +14,27 @@
 </script>
 
 <Protected>
-    <div class="flex animate-fade-in">
+    <div class="overflow-y-scroll md:overflow-auto h-svh md:h-auto">
 
-        <!-- Sidebar -->
-        <Sidebar />
+            <div class="flex animate-fade-in">
 
-        <!-- Main content, has margin left (md:ml-64) on sizes larger than md to account for sidebar -->
-        <main class="flex-1 p-5 md:p-10 md:ml-64">
-            {@render children()}
-        </main>
+                <!-- Sidebar -->
+                <Sidebar />
 
-        <!-- Settings modal -->
-        {#if $viewSettings}
-            <Modal title="Settings" closeModal={closeSettingsModal}>
-                <p class="text-muted-foreground mb-2">Signed in as:</p>
-                <p class="mb-1">{$userProfile.firstName} {$userProfile.lastName}</p>
-                <p class="mb-4">{$userProfile.email}</p>
-            </Modal>
-        {/if}
+                <!-- Main content, has margin left (md:ml-64) on sizes larger than md to account for sidebar -->
+                <main class="flex-1 p-5 md:p-10 md:ml-64">
+                    {@render children()}
+                </main>
+
+                <!-- Settings modal -->
+                {#if $viewSettings}
+                    <Modal title="Settings" closeModal={closeSettingsModal}>
+                        <p class="text-muted-foreground mb-2">Signed in as:</p>
+                        <p class="mb-1">{$userProfile.firstName} {$userProfile.lastName}</p>
+                        <p class="mb-4">{$userProfile.email}</p>
+                    </Modal>
+                {/if}
+            </div>
+
     </div>
 </Protected>
