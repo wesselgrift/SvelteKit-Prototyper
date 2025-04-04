b5023315dcc7253421c1f6e6b97f8e118cbd0eb1
Wessel Grift <wesselgrift@users.noreply.github.com>
2025-04-04 17:14:00 +0200

    add checkbox

--- src/routes/app/components/+page.svelte
+++ src/routes/app/components/+page.svelte
@@ -12,9 +12,12 @@
     import TextLink from '$lib/components/parts/TextLink.svelte';
     import FileInput from '$lib/components/parts/FileInput.svelte';
     import Textarea from '$lib/components/parts/Textarea.svelte';
+    import Checkbox from '$lib/components/parts/Checkbox.svelte';
     import SidebarAccountButton from '$lib/components/blocks/sidebar/SidebarAccountButton.svelte';
     import PageTitle from '$lib/components/parts/PageTitle.svelte';
     import { X, Maximize2, Download, Ellipsis, ArrowUpFromLine } from "lucide-svelte";
+
+    let checkedState = $state();
 
 </script>
 
@@ -73,20 +76,23 @@
         </Dialog>
     </div>
 
-    <h3 class="text-lg text-muted-foreground">Label and Input</h3>
+    <h3 class="text-lg text-muted-foreground">Form elements</h3>
     <div class="flex flex-row gap-4 items-center justify-center w-full p-5 border border-1 border-dashed border-border mb-4">
         <div class="w-full max-w-md flex flex-col gap-4">
-            <div class="flex flex-col gap-1 w-full">
+            <div class="flex flex-col gap-1">
                 <Label for="text" label="Text" />
                 <Input name="text" type="text" />
             </div>
-            <div class="flex flex-col gap-1 w-full">
+            <div class="flex flex-col gap-1">
                 <Label for="myTextarea" label="Textarea" />
                 <Textarea name="myTextarea"/>
             </div>
-            <div class="flex flex-col gap-1 w-full">
+            <div class="flex flex-col gap-1">
                 <Label for="myfiles" label="File" />
                 <FileInput name="myfiles" multiple="true"/>
+            </div>
+            <div class="flex flex-row gap-2 items-center">
+                <Checkbox name="myCheckbox"/>
             </div>
         </div>
     </div>
