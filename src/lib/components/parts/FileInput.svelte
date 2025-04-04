efe9a90a2581c4f74f993b44b7703ff756a2aab1
Wessel Grift <wesselgrift@users.noreply.github.com>
2025-04-04 16:15:46 +0200

    minor fix

--- src/lib/components/parts/FileInput.svelte
+++ src/lib/components/parts/FileInput.svelte
@@ -4,7 +4,7 @@
         multiple = false
     } = $props();
 
-    let base = "block w-full text-base lg:text-sm border border-input focus:border-input bg-background rounded-lg cursor-pointer focus-ring transition-all";
+    let base = "block overflow-hidden w-full text-base lg:text-sm border border-input focus:border-input bg-background rounded-lg cursor-pointer focus-ring transition-all";
     let button = "file:font-medium file:cursor-pointer file:border-0 file:py-2 file:px-3.5 lg:file:py-3 lg:file:px-4 file:mr-4 file:bg-secondary hover:file:bg-secondary-foreground/10 file:text-secondary-foreground";
 
 </script>
