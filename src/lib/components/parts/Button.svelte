6a25ddca972dbefd6b3233cc4fd67ba31272b822
Wessel Grift <wesselgrift@users.noreply.github.com>
2025-05-20 17:07:38 +0200

    add env example

--- src/lib/components/parts/Button.svelte
+++ src/lib/components/parts/Button.svelte
@@ -11,50 +11,75 @@
         element = $bindable()
     } = $props();
 
-    // Default classes
-    const defaultClasses = 
-        "relative overflow-hidden gap-2 inline-flex items-center justify-center whitespace-nowrap rounded-lg font-medium disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0 focus-ring transition-all";
-
-    // Disabled classes
-    const disabledClasses = 
-        "bg-muted text-muted-foreground/70 pointer-events-none border border-muted";
-
-    // Width types
-    const widthType = {
-        fill: "w-full",
-        hug: ""
-    }
-
-    // Sizes
-    const sizes = {
-        default: "py-2.5 lg:py-3.5 px-4 lg:px-5",
-        small: "py-1 lg:py-1.5 px-2 lg:px-3",
-        icon: "p-1.5"
-    }
-
-    // Variants
-    const variants = {
-        primary: `bg-primary text-primary-foreground hover-on-primary hover-on-primary`,
-        secondary: `bg-secondary text-secondary-foreground hover-on-secondary`,
-        outline: `border border-border bg-background hover-on-background`,
-        ghost: `bg-transparent hover-on-background`,
-        disabled: `${disabledClasses}`
+    const classes = {
+        default: `
+            relative 
+            overflow-hidden 
+            inline-flex gap-2 items-center justify-center whitespace-nowrap 
+            rounded-lg 
+            font-medium 
+            disabled:pointer-events-none 
+            disabled:opacity-50 
+            focus-ring 
+            transition-all
+            [&_svg]:pointer-events-none 
+            [&_svg]:shrink-0 
+        `,
+        variant: {
+            primary: `
+                bg-primary 
+                text-primary-foreground 
+                hover-on-primary 
+                hover-on-primary
+            `,
+            secondary: `
+                bg-secondary 
+                text-secondary-foreground 
+                hover-on-secondary
+            `,
+            outline: `
+                border border-border 
+                bg-background 
+                hover-on-background
+            `,
+            ghost: `
+                bg-transparent 
+                hover-on-background
+            `,
+            disabled: `
+                bg-muted 
+                text-muted-foreground/70 
+                pointer-events-none 
+                border 
+                border-muted
+            `,
+        },
+        size: {
+            default: `
+                py-2.5  lg:py-3.5 
+                px-4    lg:px-5
+            `,
+            small: `
+                py-1    lg:py-1.5 
+                px-2    lg:px-3
+            `,
+            icon: `
+                p-1.5
+            `
+        },
+        width: {
+            fill: `w-full`,
+            hug: ``
+        }
     }
 
 </script>
-  
-<button {type} onclick={onclick} class={
-    (disabled ? disabledClasses : variants[variant])
-        +' '+ 
-    sizes[size] 
-        +' '+ 
-    widthType[width] 
-        +' '+ 
-    defaultClasses
-    }
-    bind:this={element}
->
-    {#if children}
-        {@render children()}
-    {/if}
+
+<button {type} bind:this={element} onclick={onclick} class={
+        (disabled ? classes.variant.disabled : classes.variant[variant]) + ' ' +
+        classes.size[size] + ' ' +
+        classes.width[width] + ' ' +
+        classes.default 
+}>
+    {@render children()}
 </button>
--- src/lib/components/parts/Card.svelte
+++ src/lib/components/parts/Card.svelte
@@ -1,13 +1,19 @@
 <script>
     // Props
     let { children } = $props();
+
+    const classes = {
+        card: `
+            bg-card
+            text-card-foreground 
+            p-6 rounded-xl shadow-sm 
+            border 
+            border-border 
+            dark:border-border/60
+        `
+    }
 </script>
   
-<div class="
-    card p-6 rounded-xl shadow-sm border 
-    border-border dark:border-border/60
-    text-card-foreground
-    bg-card
-">
+<div class={classes.card}>
     {@render children()}
 </div>
