9057f31fe29ee0d5142bb1939e5070a39fd4d48a
Wessel Grift <wesselgrift@users.noreply.github.com>
2025-09-13 21:52:58 +0200

    Added example for dynamic routing

--- src/lib/components/parts/SidebarContent.svelte
+++ src/lib/components/parts/SidebarContent.svelte
@@ -12,7 +12,18 @@
     }
 </script>
 
-<!-- Navigation links with active state styling -->
-<!-- Active link is highlighted when current page matches the href -->
+
+<!-- Filesystem routing -->
+<span class="text-xs font-medium text-muted-foreground">Filesystem routing</span>
+
 <a class="font-medium { page.url.pathname === '/app' ? 'text-primary underline' : '' }" href="/app" onclick={handleMenuItemClick}>Hello World</a>
 <a class="font-medium { page.url.pathname === '/app/components' ? 'text-primary underline' : '' }" href="/app/components" onclick={handleMenuItemClick}>Components</a>
+
+<!-- Dynamic routes -->
+<span class="text-xs font-medium text-muted-foreground mt-5">Dynamic routes</span>
+
+<div class="flex flex-col gap-4">
+    <a class="font-medium { page.url.pathname === '/app/dynamic-route/howdy' ? 'text-primary underline' : '' }" href="/app/dynamic-route/howdy" onclick={handleMenuItemClick}>Howdy!</a>
+    <a class="font-medium { page.url.pathname === '/app/dynamic-route/cowboy' ? 'text-primary underline' : '' }" href="/app/dynamic-route/cowboy" onclick={handleMenuItemClick}>Cowboy!</a>
+    <a class="font-medium { page.url.pathname === '/app/dynamic-route/how-are-ya' ? 'text-primary underline' : '' }" href="/app/dynamic-route/how-are-ya" onclick={handleMenuItemClick}>How are ya?</a>
+</div>
---
+++ src/routes/app/dynamic-route/[slug]/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    import { page } from '$app/state';
+    import PageTitle from "$lib/components/parts/PageTitle.svelte";
+    
+    // Get the dynamic parameter from the URL
+    const slug = $derived(page.params.slug);
+  </script>
+  
+  <PageTitle title="Dynamic route: {slug}" />
+  
+  <p>You're viewing page with slug: <strong>{slug}</strong></p>
+  <p>This is a dynamic route with a parameter, slug, that can be used to load data dynamically when a user requests a page like /project/{slug}</p>
