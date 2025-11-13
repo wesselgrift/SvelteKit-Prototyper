e00c5c1ae935b5bbef58c6d7b04ec7875f7f402f
Wessel Grift <wesselgrift@users.noreply.github.com>
2025-11-13 19:13:17 +0100

    small fix

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -3,7 +3,7 @@
     import Button from "$lib/components/parts/Button/Button.svelte";
 </script>
 
-<div class="h-screen w-full flex justify-center items-center flex-col gap-4 text-foreground">
+<div class="h-screen w-full flex justify-center items-center flex-col gap-4 text-foreground animate-fade-in">
     <Telescope size="40" strokeWidth="1.2" />
     <p class="font-regular text-base">
         Whoops, we couldn't find that page.
